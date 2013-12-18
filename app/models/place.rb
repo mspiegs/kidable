e8df15ab5b6284990e13dc3cbd879e47976359c1
mspiegs <matt@mycaseinc.com>
2013-12-18 10:08:44 -0800

    Fixed new form added before_save scoring

--- app/models/place.rb
+++ app/models/place.rb
@@ -4,7 +4,7 @@ class Place < ActiveRecord::Base
 	has_many :ratings
 	geocoded_by :address_create
 
-	after_save :scoreme
+	before_save :scoreme_new
 	after_validation :geocode
 
 	scope :san_diego, -> { where("city like ?", "%San Diego%") }
@@ -23,6 +23,28 @@ class Place < ActiveRecord::Base
 		Float(self.ratings.sum(:score)) / Float(self.ratings.size)
 	end
 
+	def scoreme_new
+			kid_menu = self.kid_menu ? 80 : 20
+
+			self.boosters ? boosters = 50 : boosters = 25
+			self.high_chairs ? high_chairs = 100 : high_chairs = 5
+			self.changing_table ? changing_table = 90 : changing_table = 15
+			noise = self.noise_level.to_i * 20
+			if self.average_rating.nan?
+				place_rating = 25
+			elsif self.average_rating == 0.0
+				place_rating = 25
+			else 
+				place_rating = self.average_rating * 40.0
+			end
+				
+			total = kid_menu + boosters + noise + high_chairs + changing_table + place_rating
+
+			score1 = (Float(total)/620.0) * 100
+			self.score = score1
+	end
+
+
 	def scoreme
 		kid_menu = self.kid_menu ? 80 : 20
 
@@ -37,8 +59,9 @@ class Place < ActiveRecord::Base
 		end
 		total = kid_menu + boosters + noise + high_chairs + changing_table + rating
 
-		score = (Float(total)/620.0) * 100 
-		self.update_columns(score: score)
+		score1 = (Float(total)/620.0) * 100 
+
+		self.update_columns(score: score1)
 
 
 	end
